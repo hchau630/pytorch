288d9d54a524de620463c2fc15f11e63837c53d3
Ke Wen <kw2501@fb.com>
2023-03-17 16:36:50 +0000

    Use workCleanupLoop instead of ncclCommWatchdogInternal

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.hpp
@@ -153,6 +153,9 @@ class TORCH_API ProcessGroupNCCL : public Backend {
     // and False otherwise.
     bool timedOut();
 
+    // Helper function that checks timeout and set exception
+    void checkTimeout();
+
     std::vector<at::Tensor> result() override;
 
    protected:
@@ -557,6 +560,10 @@ class TORCH_API ProcessGroupNCCL : public Backend {
 
   void workCleanupLoop();
 
+  void logWorkStart(WorkNCCL& work);
+
+  void logWorkEnd(WorkNCCL& work);
+
  protected:
   static const int64_t kWatchdogThreadSleepMillis;
   static const int64_t kWorkCleanupThreadSleepMillis;
@@ -707,6 +714,8 @@ class TORCH_API ProcessGroupNCCL : public Backend {
 
   // Counting for the sequential number of NCCL collective call.
   uint64_t seq_{0};
+
+  std::exception_ptr watchDogException_ = nullptr;
 
 #ifdef USE_NCCL_WITH_UCC
   // ProcessGroupUCC shared library handle and ProcessGroup pointer
